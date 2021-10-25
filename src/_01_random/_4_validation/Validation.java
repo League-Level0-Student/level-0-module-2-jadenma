a3f8891657df34c12aed8d4042b7b27736fa3732
student <student@ip-172-31-59-75.us-west-2.compute.internal>
2021-10-25 16:59:15 -0700

    did dice roll and validation and started magic 8-ball

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -11,13 +11,24 @@ public class Validation {
 	public static void main(String[] args) {
 		
 		Random randomMaker = new Random();
-		
-		int randomNumber = randomMaker.nextInt(5);
-		
-		System.out.println(randomNumber);
-
+		int count = 0;
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		while (count <10) {
+			int randomNumber = randomMaker.nextInt(5);
+			System.out.println(randomNumber);
+if (randomNumber == 0) { 
+	System.out.println("You're amazing!!");
+} else if (randomNumber == 1) { 
+	System.out.println("You're nice!!");
+} else if (randomNumber == 2) { 
+	System.out.println("You're smart!!");
+} else if (randomNumber == 3) { 
+	System.out.println("You rock!!");
+} else { 
+	System.out.println("You're strong!!");
+}
+count++;
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -6,17 +6,18 @@ package _01_random._5_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below
-
+	public static void main(String[] args) {
 	// 2. Get the user to enter a question for the 8 ball to answer
-	
+	String question = JOptionPane.showInputDialog("Enter a question.");
 
 	// 3. Make a variable and initialize it to a random number.
 	//     ** You will need to make a random object!
 	//     Limit the random numbers to be between 0 and 3
-
 	
 	// 4. If the random number is 0
 
@@ -34,4 +35,5 @@ public class Magic8Ball {
 
 	// -- write your own answer
 
+}
 }
